1d5a3d26db36da851768b4bfa7dde902b2eae0b2
Kristina Diaz <kristina.diaz84@gmail.com>
2021-11-10 11:56:42 -0700

    created working API, fetch, & first  deliverable request. started working on event listener.

--- src/index.js
+++ src/index.js
@@ -1,2 +1,33 @@
 // Your code here
 // checking if forked correctly
+
+const charAPI = 'http://localhost:3000';
+
+fetch(`${charAPI}/characters`)
+.then(res => res.json())
+.then(json => {
+    json.forEach(renderCharacter)
+});
+
+const charContainer = document.getElementById('character-bar');
+
+function renderCharacter(characters) {
+    const span = document.createElement('span');
+
+    span.textContent = characters.name;
+    charContainer.append(span)
+};
+
+const charInfo = renderCharacter;
+charInfo.addEventListener('click', displayInfo);
+
+function displayInfo(event) { 
+    event.preventDefault();
+}
+// create a click event listener on character bar
+// once clicked, call function that will display characters.id in div#detailed-info
+
+// 1. create variable that holds renderCharacter
+// 2. add eventListener to variable
+// 3. create function that will GET information from API
+// 4. eventListener will call that function with the info
